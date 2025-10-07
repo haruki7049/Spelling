33c440108b8a0a6a5328056ce86b190d862f162a
haruki7049 <tontonkirikiri@gmail.com>
2025-10-07 11:17:19 +0900

    Init repo

---
+++ src/main/java/dev/haruki7049/Spelling/Main.java
@@ -0,0 +1,21 @@
+package dev.haruki7049.Spelling;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Main extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("BasicGameSample");
+    }
+
+    @Override
+    protected void initGame() {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
