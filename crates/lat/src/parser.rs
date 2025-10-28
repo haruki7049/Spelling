7b8e14d1bb4301a12863c3aa8b62529aefd6647a
haruki7049 <tontonkirikiri@gmail.com>
2025-10-28 11:50:55 +0900

    feat(lat): Create parse function & LatValue

---
+++ crates/lat/src/parser.rs
@@ -0,0 +1,5 @@
+use crate::LatValue;
+
+pub fn parse(_input: &str) -> LatValue {
+    LatValue {}
+}
--- crates/lat/tests/syntaxes.rs
+++ crates/lat/tests/syntaxes.rs
@@ -1,8 +1,11 @@
 mod common_syntaxes {
+    use lat::{LatValue, parse};
+
     #[test]
     fn test() {
         const INPUT: &str = "";
+        let parsed_result: LatValue = parse(INPUT);
 
-        assert_eq!(INPUT, "");
+        assert_eq!(parsed_result, LatValue {});
     }
 }
