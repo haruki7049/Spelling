890b9904cc6d886d8015e612b721cb7d2ef1aab4
haruki7049 <tontonkirikiri@gmail.com>
2026-01-22 13:28:02 +0900

    Update

--- crates/lat/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let target = std::env::var("TARGET").unwrap();
-    if target != "wasm32-wasip2" {
-        println!("cargo::warning=This crate should be built for wasm32-wasip2");
-    }
-}
