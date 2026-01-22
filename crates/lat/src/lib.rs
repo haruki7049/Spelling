666f0d11c392c8f4b729ec4410ec09d2d29f31ad
haruki7049 <tontonkirikiri@gmail.com>
2026-01-22 13:44:57 +0900

    Update

--- crates/lat/src/lib.rs
+++ crates/lat/src/lib.rs
@@ -1,14 +1,32 @@
 wit_bindgen::generate!({
     world: "lat",
+    additional_derives: [PartialEq, Eq, Hash, Clone],
 });
 
 pub struct Lat;
 
 impl Guest for Lat {
     fn parse(inputs: String) -> Result<LatValue, ParseError> {
-        dbg!(inputs);
-        todo!();
+        Ok(LatValue::new(inputs))
+    }
+}
+
+impl LatValue {
+    pub fn new(value: String) -> Self {
+        Self { value }
     }
 }
 
 export!(Lat);
+
+#[cfg(test)]
+mod tests {
+    use super::{Guest, Lat, LatValue};
+
+    #[test]
+    fn lat_parse() {
+        let inputs: String = "HELLO".to_string();
+
+        assert_eq!(Lat::parse(inputs.clone()), Ok(LatValue::new(inputs)));
+    }
+}
