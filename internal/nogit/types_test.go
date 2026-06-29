0b0fdd612d08c2703fc2f3aa35511bfaee2a2197
haruki7049 <tontonkirikiri@gmail.com>
2026-06-29 11:00:03 +0900

    Update

--- internal/nogit/types_test.go
+++ internal/nogit/types_test.go
@@ -4,61 +4,63 @@ import (
 	"testing"
 )
 
-func testParseFromText(t *testing.T, text string) {
-	result, err := Parse(text)
+func testParseFromText(t *testing.T, text string, expected *NogitAST) {
+	actual, err := Parse(text)
 	if err != nil {
 		t.Errorf("An error invoked from Parse func: %v", err)
 	}
 
-	if result == nil {
-		t.Error("The result by Parse func is nil")
+	if actual == nil {
+		t.Error("The actual variable created by Parse func is nil")
+	}
+
+	if len(expected.Words) != len(actual.Words) {
+		t.Errorf("The expected words' len is %d, but got %d", len(expected.Words), len(actual.Words))
+	}
+
+	for i := range expected.Words {
+		expectedWord := expected.Words[i]
+		actualWord := actual.Words[i]
+
+		if expectedWord.Grammatical != "" {
+			if expectedWord.Grammatical != actualWord.Grammatical {
+				t.Errorf("Expected %#v, but got %#v", expectedWord.Grammatical, actualWord.Grammatical)
+			}
+		} else {
+			if expectedWord.Noun != actualWord.Noun {
+				t.Errorf("Expected %#v, but got %#v", expectedWord.Noun, actualWord.Noun)
+			}
+		}
 	}
 }
 
 func TestParse(t *testing.T) {
-	testParseFromText(t, "watashi migi hidari")
-	testParseFromText(t, "watashi ha migi ni")
-	testParseFromText(t, "watashi ha hidari ni ugoku")
+	testParseFromText(t, "watashi migi hidari", &NogitAST{Words: []*Word{
+		{Noun: "watashi"},
+		{Noun: "migi"},
+		{Noun: "hidari"},
+	}})
 
-	{
-		text := "watashihahidariniugoku"
-		actual, err := Parse(text)
+	testParseFromText(t, "watashi ha migi ni", &NogitAST{Words: []*Word{
+		{Noun: "watashi"},
+		{Grammatical: "ha"},
+		{Noun: "migi"},
+		{Grammatical: "ni"},
+	}})
 
-		if err != nil {
-			t.Errorf("An error invoked from Parse func: %v", err)
-		}
+	testParseFromText(t, "watashi ha hidari ni ugoku", &NogitAST{Words: []*Word{
+		{Noun: "watashi"},
+		{Grammatical: "ha"},
+		{Noun: "hidari"},
+		{Grammatical: "ni"},
+		{Noun: "ugoku"},
+	}})
 
-		if actual == nil {
-			t.Error("The result by Parse func is nil")
-		}
-
-		expected := &NogitAST{
-			Words: []*Word{
-				{Noun: "watashi"},
-				{Grammatical: "ha"},
-				{Noun: "hidari"},
-				{Grammatical: "ni"},
-				{Noun: "ugoku"},
-			},
-		}
-
-		if len(expected.Words) != len(actual.Words) {
-			t.Errorf("The expected words' len is %d, but got %d", len(expected.Words), len(actual.Words))
-		}
-
-		for i := range expected.Words {
-			expectedWord := expected.Words[i]
-			actualWord := actual.Words[i]
-
-			if expectedWord.Grammatical != "" {
-				if expectedWord.Grammatical != actualWord.Grammatical {
-					t.Errorf("Expected %#v, but got %#v", expectedWord.Grammatical, actualWord.Grammatical)
-				}
-			} else {
-				if expectedWord.Noun != actualWord.Noun {
-					t.Errorf("Expected %#v, but got %#v", expectedWord.Noun, actualWord.Noun)
-				}
-			}
-		}
-	}
+	testParseFromText(t, "watashihahidariniugoku", &NogitAST{Words: []*Word{
+		{Noun: "watashi"},
+		{Grammatical: "ha"},
+		{Noun: "hidari"},
+		{Grammatical: "ni"},
+		{Noun: "ugoku"},
+	}})
 }
